0ab9c8e3fc35032cc8397542b4610ff9caa15bae
Danil Zalizchuk <danilmail0110@gmail.com>
2023-01-31 19:47:44 +0200

    add button that increments a value with state

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,8 @@
 <script>
-  let name = 'Svelte';
-  let src = 'https://dummyimage.com/200x150?text=random';
-  let injected = '<input type="password" name="password">';
+  import Counter from './lib/Counter.svelte';
 </script>
 
-<h1>Hello, {name}!</h1>
-<img {src} alt="Svelte text is so {name}'ish!" />
-<div>{@html injected}</div>
+<Counter />
 
 <style>
-  h1 {
-    color: orangered;
-  }
 </style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,20 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
+  let count = 0;
+
+  // known problem - string isn't being updated when count updates
+  let string = `You have clicked ${count} times.`;
+
+  function increment() {
+    count++;
   }
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<button on:click={increment}>Clicks: {count}</button>
+<div>{string}</div>
+
+<style>
+  button {
+    padding: 10px 15px;
+    border-radius: 5px;
+  }
+</style>
