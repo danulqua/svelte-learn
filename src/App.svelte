f7198339ea59a53f5a96834ba9965bdc27d14031
Danil Zalizchuk <danilmail0110@gmail.com>
2023-01-31 19:37:22 +0200

    add basic structure of .svelte file

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = 'Svelte';
+  let src = 'https://dummyimage.com/200x150?text=random';
+  let injected = '<input type="password" name="password">';
+</script>
+
+<h1>Hello, {name}!</h1>
+<img {src} alt="Svelte text is so {name}'ish!" />
+<div>{@html injected}</div>
+
+<style>
+  h1 {
+    color: orangered;
+  }
+</style>
