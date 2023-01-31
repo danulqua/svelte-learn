8fb5cc481b692ebf071821740bc0939eba2594e1
Danil Zalizchuk <danilmail0110@gmail.com>
2023-01-31 20:07:57 +0200

    learn about reactive statement caveat reactive statement updates only when the variables that are being changed appear on the right hand side of the reactive statement

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,15 +1,36 @@
 <script>
-  let count = 0;
-  // let string =`You have clicked ${count} times.`;
-  $: string = `You have clicked ${count} times.`;
+  let count1 = 0;
+  let count2 = 0;
 
-  function increment() {
-    count++;
+  // Not going to work because reactive variables doesn't appear on the
+  // right hand side of the expression
+  // $: string = `You have clicked ${sumInorrect()} times.`;
+
+  // Works because `count1` and `count2` exist on the right hand side
+  $: string = `You have clicked ${sumCorrect(count1, count2)} times.`;
+
+  function increment1() {
+    count1++;
+  }
+
+  function increment2() {
+    count2++;
+  }
+
+  // Not going to work in the reactive `$:` statement
+  // function sumIncorrect() {
+  //   return count1 + count2;
+  // }
+
+  // Works in the reactive `$:` statement
+  function sumCorrect(a, b) {
+    return a + b;
   }
 </script>
 
-<button on:click={increment}>Clicks: {count}</button>
-<div>{string}</div>
+<button on:click={increment1}>Clicks: {count1}</button>
+<button on:click={increment2}>Clicks: {count2}</button>
+<p>{string}</p>
 
 <style>
   button {
