4cd54418a4e05f8c914692f1ef95feb621859a2c
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-01 15:15:47 +0200

    learn about binding input values

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -1,10 +1,24 @@
 <script>
   export let todos = [];
+
+  let inputText = '';
+
+  function handleAddTodo() {
+    console.log(inputText);
+  }
 </script>
 
-<ul>
-  {#each todos as { id, title }, idx (id)}
-    {@const todoNumber = idx + 1}
-    <li>{todoNumber} - {title}</li>
-  {/each}
-</ul>
+<div class="todo-list-wrapper">
+  <ul>
+    {#each todos as { id, title }, idx (id)}
+      {@const todoNumber = idx + 1}
+
+      <li>{todoNumber} - {title}</li>
+    {/each}
+  </ul>
+
+  <form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
+    <input type="text" bind:value={inputText} />
+    <button type="submit">Add todo</button>
+  </form>
+</div>
