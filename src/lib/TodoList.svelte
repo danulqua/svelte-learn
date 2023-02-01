5e5627d04e15d055d0f54233ea4662e537463222
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-01 19:28:21 +0200

    use block dimensions binding to get scrollHeight for autoscroll

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -4,7 +4,6 @@
   import { afterUpdate, createEventDispatcher } from 'svelte';
   import Button from './Button.svelte';
 
-  // todos and previous todos
   export let todos = [];
   let prevTodos = todos;
   let autoscroll = false;
@@ -12,30 +11,23 @@
   export const clearInput = () => (inputText = '');
   export const focusInput = () => input.focus();
 
-  // In a reactive statement compare how exactly new `todos` has been changed.
-  // If items count in the array has been increased - it means that new item
-  // was added, so we need to scroll. For that, we're setting the `autoscroll`
-  // flag to `true`. After if statement, we're updating `prevTodos` with new `todos`.
   $: {
     if (todos.length > prevTodos.length) autoscroll = true;
     prevTodos = todos;
   }
 
-  // `afterUpdate` method fires right after the DOM updates, so it's where we need
-  // to apply the scroll logic.
   afterUpdate(() => {
-    // If `autoscroll` is set to `true` - we should scroll to the bottom where is
-    // the new item appeared and set `autoscroll` back to `false`.
     if (autoscroll) {
-      listDiv.scrollTo({ top: listDiv.scrollHeight, behavior: 'smooth' });
+      listDiv.scrollTo({ top: listDivScrollHeight, behavior: 'smooth' });
       autoscroll = false;
     }
   });
 
   const dispatch = createEventDispatcher();
-  let input;
-  let inputText = '';
-  let listDiv;
+  let input,
+    inputText = '',
+    listDiv,
+    listDivScrollHeight;
 
   function handleAddTodo() {
     const isNotPrevented = dispatch('addtodo', inputText, {
@@ -54,19 +46,22 @@
   }
 </script>
 
+{listDivScrollHeight}
 <div class="todo-list-wrapper">
   <div class="todo-list" style:max-height="150px" style:overflow="auto" bind:this={listDiv}>
-    <ul>
-      {#each todos as { id, title, completed } (id)}
-        <li>
-          <label>
-            <input type="checkbox" checked={completed} on:change={() => handleToggleTodo(id)} />
-            {title}
-            <button on:click={() => handleDeleteTodo(id)}>Delete</button>
-          </label>
-        </li>
-      {/each}
-    </ul>
+    <div bind:offsetHeight={listDivScrollHeight}>
+      <ul>
+        {#each todos as { id, title, completed } (id)}
+          <li>
+            <label>
+              <input type="checkbox" checked={completed} on:change={() => handleToggleTodo(id)} />
+              {title}
+              <button on:click={() => handleDeleteTodo(id)}>Delete</button>
+            </label>
+          </li>
+        {/each}
+      </ul>
+    </div>
   </div>
 
   <form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
